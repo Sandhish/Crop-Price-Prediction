b45488ab25a3357ba02cd1e8b27d7443639f3949
Sandhish <sandhish19@gmail.com>
2024-08-17 00:39:02 +0530

    ui changes

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -3,7 +3,7 @@ import styles from './home.module.css'
 const Home = () => {
   return (
     <div id='home' className={styles.heroContainer}>
-        <img className={styles.heroImg} src="src/assets/mainPic.jpg" alt="heroPic" />
+        <img className={styles.heroImg} src="https://i.imgur.com/nMsCqdy.jpeg" alt="heroPic" />
         <span className={styles.heroSub}>Sowing Data,<br/>Reaping Insights!</span>
     </div>
   )
